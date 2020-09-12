f55ecba3c296d8b5e41b9f9aadc3b6a6fbd6b1c5
Jormin <jorminxee@gmail.com>
2020-09-12 17:57:11 +0800

    init

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://api.im.local.com:8000/api/"',
+  BASE_API: '"http://localhost:8000/api/"',
 })
